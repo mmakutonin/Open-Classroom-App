b946736dee4a74666399dceeb2104c456f613e3e
adelvento1 <ashleydelvento@gmail.com>
2019-09-30 12:37:18 -0400

    Editing the beforeConnect to fix Heroku Error

--- config/sockets.js
+++ config/sockets.js
@@ -42,13 +42,13 @@ module.exports.sockets = {
   *                                                                          *
   ***************************************************************************/
 
-  // beforeConnect: function(handshake, proceed) {
-  //
-  //   // `true` allows the socket to connect.
-  //   // (`false` would reject the connection)
-  //   return proceed(undefined, true);
-  //
-  // },
+   beforeConnect: function(handshake, proceed) {
+  
+    // `true` allows the socket to connect.
+    // (`false` would reject the connection)
+     return proceed(undefined, true);
+  
+   },
 
 
   /***************************************************************************
@@ -76,7 +76,7 @@ module.exports.sockets = {
   *                                                                          *
   ***************************************************************************/
 
-  // grant3rdPartyCookie: true,
+   grant3rdPartyCookie: true,
 
 
 };
