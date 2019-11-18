9d28f15ae3846f7d896cb1424665f55028fd81aa
mmakutonin <michael.makutonin@gmail.com>
2019-11-17 21:36:17 -0500

    Removed twillioVideo due to WebRTC dependency.

--- openclassroom/Components/react-components/RoomScreen.js
+++ openclassroom/Components/react-components/RoomScreen.js
@@ -1,104 +1,26 @@
-import React, { Component } from 'react';
-import { Text, View, TouchableOpacity, Modal } from 'react-native';
-import ServerFunctions from '../util/server-functions';
-import {
-  TwilioVideoLocalView,
-  TwilioVideoParticipantView,
-  TwilioVideo
-} from 'react-native-twilio-video-webrtc';
+import React from 'react'
+import { WebView } from 'react-native-webview'
+import ServerFxns from '../util/server-functions'
 
-export default class RoomScreen extends Component {
-
-    //props: token, userName, password, isAdmin, roomName
-
+export default class Room extends React.Component {
     constructor(props) {
-        super(props);
+        super(props)
 
         this.state = {
-            isAudioEnabled: this.props.isAdmin,
-            isVideoEnabled: this.props.isAdmin,
-            status: '',
-            videoTracks: [],
-            errorModal: false,
-            errorMessage: ''
+            username: this.props.navigation.getParam('username', ''),
+            password: this.props.navigation.getParam('password', ''),
+            token: this.props.navigation.getParam('token', ''),
+            roomName: this.props.navigation.getParam('name', ''),
         }
+
     }
 
-    componentDidMount() {
-        this.refs.twilioVideo.connect({ roomName: this.props.roomName, accessToken: this.props.token })
-        this.setState({status: 'connecting'})
-    }
-
-    _onRoomDidConnect() {
-        this.setState({status:'connected'})
-    }
-
-    _onRoomDidFailToConnect() {
-        this.setState({
-            errorModal: true,
-            errorMessage: 'Room failed to connect. Please check you room name and try again.'
-        })
-    }
-
-    _onRoomDidDisconnect() {
-        this.refs.twilioVideo.connect({ roomName: this.props.roomName, accessToken: this.props.token })
-        this.setState({status: 'connecting'})
-    }
-
-    async _onExitRoom() {
-        if(this.props.isAdmin) {
-            const roomSid = await ServerFunctions.listRooms(this.props.username, this.props.password, false, this.props.roomName)
-                                    .then(res => res.res[0].sid)
-            ServerFunctions.closeRoom(this.props.username, this.props.password, roomSid)
-        }
-        this.props.navigation.navigate('MainPage')
-    }
-
-    _onParticipantAddedVideoTrack({participant, track}) {
-        console.log(participant, track);
-
-        this.setState({videoTracks: [
-            ...this.state.videoTracks,
-            {
-                participantSid: participant.sid,
-                trackSid: track.sid
-            }
-        ]})
-    }
     render() {
         return (
-            <View>
-                <Modal visible = {this.state.errorModal} >
-                    <TouchableOpacity>
-                        <Text>Room Connection Failed, please try again. </Text>
-                    </TouchableOpacity>
-                </Modal>
-
-                {
-                    this.state.status === 'connected' &&
-                    <View>
-                        {
-                        this.state.videoTracks.map(({participantSid, trackSid}) => {
-                            return (
-                                <TwilioVideoParticipantView
-                                    key={trackSid}
-                                    trackIdentifier={{participantIdentity: participantSid, videoTrackId: trackSid}}
-                                />
-                                )
-                        })
-                        }
-                    </View>
-                }
-                <TwilioVideoLocalView enabled={true} />
-
-                <TwilioVideo
-                    ref="twilioVideo"
-                    onRoomDidConnect={ this._onRoomDidConnect }
-                    onRoomDidDisconnect={ this._onRoomDidDisconnect }
-                    onRoomDidFailToConnect= { this._onRoomDidFailToConnect }
-                    onParticipantAddedVideoTrack={ this._onParticipantAddedVideoTrack }
-                />
-            </View>
-        )
+            <WebView
+              source={{ uri: ServerFxns.serverUrl }}
+              style={{ marginTop: 20 }}
+            />
+          );
     }
 }
